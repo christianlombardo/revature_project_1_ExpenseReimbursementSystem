a9b925cfdd78c4ebd76bea9e9345f999b86172db
joseph-malak <92759345+joseph-malak@users.noreply.github.com>
2021-11-20 13:59:01 -0500

    Update EmployeeViewPendingRequestsServlet.java

--- main/java/EmployeeViewPendingRequestsServlet.java
+++ main/java/EmployeeViewPendingRequestsServlet.java
@@ -5,7 +5,52 @@ import javax.servlet.http.HttpServletResponse;
 public class EmployeeViewPendingRequestsServlet extends HttpServlet {
 
     public void doPost (HttpServletRequest request, HttpServletResponse reponse) {
+        
+         out.println(
+                "<ul class=\"nav nav-tabs\"navbar-fixed-top sticky>\n" +
+                        "  <li class=\"nav-item\">\n" +
+                        "    <a class=\"nav-link active\" aria-current=\"page\" href=\"#\">Home</a>\n" +
+                        "  </li>\n" +
+                        "  <li class=\"nav-item\">\n" +
+                        "    <a class=\"nav-link\" href=\"#\">Submit</a>\n" +
+                        "  </li>\n" +
+                        "  <li class=\"nav-item\">\n" +
+                        "    <a class=\"nav-link\" href=\"#\">View all current reimbursements</a>\n" +
+                        "  </li>\n" +
+                        "  <li class=\"nav-item\">\n" +
+                        "    <a class=\"nav-link\" href=\"#\">View all past reimbursements</a>\n" +
+                        "  </li>\n" +
+                        "</ul>" +
+                "<style>body{background-color:grey}</style>" +
+                "<header>head</head>" +
+                "<div class=\"container\"" +
+                "<div class=\"row g-3\">" +
+                "<div class=\"col-md-12\">" +
+                "	<h1>User Profile</h1>" +
+                "</div>" +
+                "<div class=\"col-md-12\">" +
+                "<table class=\"table table-bordered table-dark table-responsive\" style=\"font-family:Arial;\">" +
+                "<tr>\n" +
+                "    <th>ID</th>\n" +
+                "    <th>Name</th>\n" +
+                "    <th>Username</th>\n" +
+                "  </tr>");
+
+        HttpSession httpSession = request.getSession();
+        for (Employee employees : results) {
+            httpSession.setAttribute("id", employees.getId());
+            out.println("<tr>\n" +
+                    "    <td>" + employees.getId()+ "</td>\n" +
+                    "    <td>" + employees.getName() + "</td>\n" +
+                    "    <td>" + employees.getUsername() + "</td>\n" +
+                    "</tr>" );
+
+        }
+        out.println("</table>" +
+                "</div>" +
+                "</div></div>");
+
+        request.getRequestDispatcher("footer.html").include(request, response);
 
     }
-
 }

51b84e0ba227361d4147a351b0b088fc7f2a8f6e
Christian Lombardo <christianlombardo@verizon.net>
2021-11-20 13:32:45 -0500

    Pending servlet.

---
+++ main/java/EmployeeViewPendingRequestsServlet.java
@@ -0,0 +1,11 @@
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class EmployeeViewPendingRequestsServlet extends HttpServlet {
+
+    public void doPost (HttpServletRequest request, HttpServletResponse reponse) {
+
+    }
+
+}

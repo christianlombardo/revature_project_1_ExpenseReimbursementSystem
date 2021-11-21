9c0b3437fe4a9c03ef81ac226f72289bf07db301
joseph-malak <92759345+joseph-malak@users.noreply.github.com>
2021-11-21 16:13:58 -0500

    Update EmployeeViewPendingRequestsServlet.java

--- main/java/EmployeeViewPendingRequestsServlet.java
+++ main/java/EmployeeViewPendingRequestsServlet.java
@@ -16,39 +16,36 @@ import java.util.List;
 public class EmployeeViewPendingRequestsServlet extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        response.setContentType("text/html");
+       response.setContentType("text/html");
         PrintWriter out = response.getWriter();
 
         ReimbursementDao daoReimbursement = ReimbursementDaoFactory.getEmployeeDao();
+        EmployeeDao employeeDao = EmployeeDaoFactory.getEmployeeDao();
         request.getRequestDispatcher("test.html").include(request, response);
 
-         out.println(
-            
-                "<style>body{background-color:grey}</style>" +
-                "<header>head</head>" +
-                "<div class=\"container\"" +
-                "<div class=\"row g-3\">" +
-                "<div class=\"col-md-12\">" +
-                "	<h1>User Profile</h1>" +
-                "</div>" +
-                "<div class=\"col-md-12\">" +
-                "<table class=\"table table-bordered table-dark table-responsive\" style=\"font-family:Arial;\">" +
-                "<tr>\n" +
-                "    <th>Ticket Number</th>\n" +
-                "    <th>Expense Detail</th>\n" +
-                "    <th>Amount</th>\n" +
-                "    <th>Date Start</th>\n" +
-                "    <th>Date End</th>\n" +
-                "    <th>Ticket Status</th>\n" +
-                "  </tr>");
+        out.println(
+
+                        "<style>body{background-color:grey}</style>" +
+                        "<div class=\"container\"" +
+                        "<div class=\"row g-3\">" +
+                        "<div class=\"col-md-12\">" +
+                        "	<h1>User Profile</h1>" +
+                        "</div>" +
+                        "<div class=\"col-md-12\">" +
+                        "<table class=\"table table-bordered table-dark table-responsive\" style=\"font-family:Arial;\">" +
+                        "<tr>\n" +
+                        "    <th>ID</th>\n" +
+                        "    <th>Name</th>\n" +
+                        "    <th>Username</th>\n" +
+                        "  </tr>");
 
         HttpSession httpSession = request.getSession();
+
+        List<Reimbursement> results = daoReimbursement.readAll();
         Employee employee = (Employee)httpSession.getAttribute("employee");
 
-        List<Reimbursement> reimbursements = daoReimbursement.readByEmployeeTicketStatus(employee.getId(), Reimbursement.hmap.get("PENDING"));
-
-        for (Reimbursement reimbursement : reimbursements) {
-            //httpSession.setAttribute("id", reimbursement.getTicketNumber());
+        for (Reimbursement reimbursement : results) {
+            httpSession.setAttribute("id", reimbursement.getTicketNumber());
             out.println("<tr>\n" +
                     "    <td>" + reimbursement.getTicketNumber()+ "</td>\n" +
                     "    <td>" + reimbursement.getExpenseDetail() + "</td>\n" +

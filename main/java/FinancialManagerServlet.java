c1508f3182bc3080888395750a3c4fbcff03137a
joseph-malak <92759345+joseph-malak@users.noreply.github.com>
2021-11-21 15:42:13 -0500

    Update FinancialManagerServlet.java

--- main/java/FinancialManagerServlet.java
+++ main/java/FinancialManagerServlet.java
@@ -9,20 +9,56 @@ import java.util.List;
 public class FinancialManagerServlet extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-
+        int i= 1;
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
 
         // see all pending reimbursements
         List<Employee> employees = new ArrayList<>();
         List<Reimbursement> reimbursements = new ArrayList<>();
-
-
-        for (int i=0 ; i<employees.size() ; i++) {
-            Employee employee = employees.get(i);
-            Reimbursement reimbursement = reimbursements.get(i);
-            // html/css/bootstrap
+        Iterator iterator = reimbursements.iterator();
+        out.println(
+                        "<style>body{background-color:grey}</style>" +
+                        "<header>head</head>" +
+                        "<div class=\"container\"" +
+                        "<div class=\"row g-3\">" +
+                        "<div class=\"col-md-12\">" +
+                        "<h1>User Profile</h1>" +
+                        "</div>" +
+                        "<div class=\"col-md-12\">" +
+                        "<table class=\"table table-bordered table-dark table-responsive\" style=\"font-family:Arial;\">" +
+                        "<tr>\n" +
+                        "<th>ID</th>\n" +
+                        "<th>Name</th>\n" +
+                        "<th>Username</th>\n" +
+                        "<th>Amount</th>\n" +
+                        "<th>Date</th>\n" +
+                        "<th>Ticket Number</th>\n" +
+                        "<th>Ticket Status</th>\n" +
+                        "</tr>");
+        
+          while(iterator.hasNext()){
+                i++;
+                //employee
+                //employee infor
+                if (reimbursements.get(i).getEmployeeId() == employees.get(i).getId()) {
+                    out.println("<tr>\n" +
+                            "<td>" + employees.get(i).getId() + "</td>\n" +
+                            "<td>" + employees.get(i).getName() + "</td>\n" +
+                            "<td>" + employees.get(i).getUsername() + "</td>\n" +
+                            "<td>" + reimbursements.get(i).getAmount() + "</td>\n" +
+                            "<td>" + reimbursements.get(i).getDateStart() + "</td>\n" +
+                            "<td>" + reimbursements.get(i).getDateEnd() + "</td>\n" +
+                            "<td>" + reimbursements.get(i).getTicketStatus() + "</td>\n" +
+                            "</tr>");
+                }
         }
+        out.println("</table>" +
+                "</div>" +
+                "</div></div>");
+
+
+
 
     }
 

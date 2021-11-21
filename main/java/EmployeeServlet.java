6992d72e764d8c7d90dc3ec354326887ca0530ea
joseph-malak <92759345+joseph-malak@users.noreply.github.com>
2021-11-21 15:34:19 -0500

    Update EmployeeServlet.java

--- main/java/EmployeeServlet.java
+++ main/java/EmployeeServlet.java
@@ -1,3 +1,4 @@
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -7,7 +8,7 @@ import java.io.PrintWriter;
 
 public class EmployeeServlet extends HttpServlet {
 
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
 
@@ -23,42 +24,28 @@ public class EmployeeServlet extends HttpServlet {
             out.println("<h1>You have successfully logged in!</h1>");
 
             // submit requests for reimbursement
+            request.getRequestDispatcher("test.html").include(request, response);
             employee = employeeDao.getEmployeeByUsernamePassword(username, password);
             HttpSession session = request.getSession();
             session.setAttribute("employee", employee);
-            out.println("<a href='EmployeeSubmitReimbursementServlet'>Request for Reimbursement</a>" +
+            out.println(
+                    "<section class='vh-100 gradient-custom'>"+
+                    "<div class='container py-5 h-100'>"+
+                    "<div class='row d-flex justify-content-center align-items-center h-100'>"+
+                    "<div class='col-12 col-md-9 col-lg-5 col-xl-5'>"+
+                    "<div class='card bg-dark text-white style ='border-radius: 8rem;'>"+
+                    "<div class='card-body p-5 text-center'>"+
 
-            "<ul class=\"nav nav-tabs\"navbar-fixed-top sticky>\n" +
-                "  <li class=\"nav-item\">\n" +
-                "    <a class=\"nav-link active\" aria-current=\"page\" href=\"#\">Home</a>\n" +
-                "  </li>\n" +
-                "  <li class=\"nav-item\">\n" +
-                "    <a class=\"nav-link\" href=\"#\">Submit</a>\n" +
-                "  </li>\n" +
-                "  <li class=\"nav-item\">\n" +
-                "    <a class=\"nav-link\" href=\"EmployeeViewPendingRequestsServlet\">View all current reimbursements</a>\n" +
-                "  </li>\n" +
-                "  <li class=\"nav-item\">\n" +
-                "    <a class=\"nav-link\" href=\"#\">View all past reimbursements</a>\n" +
-                "  </li>\n" +
-                "</ul>" +
-                "<section class='vh-100 gradient-custom'>"+
-                "<div class='container py-5 h-100'>"+
-                "<div class='row d-flex justify-content-center align-items-center h-100'>"+
-                "<div class='col-12 col-md-9 col-lg-5 col-xl-5'>"+
-                "<div class='card bg-dark text-white style ='border-radius: 8rem;'>"+
-                "<div class='card-body p-5 text-center'>"+
+                    "<div class='mb-md-5 mt-md-4 pb-5'>"+
 
-                "<div class='mb-md-5 mt-md-4 pb-5'>"+
+                    "<h2 class='fw-bold mb-2 text'>Employee Reimbursement</h2>"+
+                    "<p class='text-white-50 mb-5'></p>"+
 
-                "<h2 class='fw-bold mb-2 text'>Employee Reimbursement</h2>"+
-                "<p class='text-white-50 mb-5'></p>"+
+                    "</div>"+
 
-                "</div>"+
+                    " <div> <p class='mb-0'>Financial Managers</p> </div>"+
 
-                " <div> <p class='mb-0'>Financial Managers</p> </div>"+
-
-                "</div> </div> </div> </div> </div> </section> </form>");
+                    "</div> </div> </div> </div> </div> </section> </form>");
 
 
             // view their past tickets and pending requests.

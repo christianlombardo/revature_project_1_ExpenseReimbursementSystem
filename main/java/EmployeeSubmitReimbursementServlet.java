42a28637477ca428f74cf5fd0e5e308366e4d0bb
joseph-malak <92759345+joseph-malak@users.noreply.github.com>
2021-11-21 16:14:58 -0500

    Update EmployeeSubmitReimbursementServlet.java

--- main/java/EmployeeSubmitReimbursementServlet.java
+++ main/java/EmployeeSubmitReimbursementServlet.java
@@ -21,52 +21,49 @@ public class EmployeeSubmitReimbursementServlet extends HttpServlet {
         HttpSession session = request.getSession(false); // true to create a new session for this request if necessary; false to return null if there's no current session https://docs.oracle.com/javaee/6/api/javax/servlet/http/HttpServletRequest.html#getSession(boolean)
         Employee employee = (Employee)session.getAttribute("employee");
 
-        out.println("id = " + employee.getId() + "username = " + employee.getUsername() + " password = " + employee.getPassword() + "");
 
         // submit request to add in the reimbursement table in the database.
 
- request.getRequestDispatcher("test.html").include(request, response);
-        
- out.println("<section class='vh-100 gradient-custom'>"+
-         "<form action=\"EmployeeSubmitReimbursementServlet\" method=\"post\" > " +
-    "<div class='container py-5 h-100'>"+
-            "<div class='row d-flex justify-content-center align-items-center h-100'>"+
+
+
+        out.println("<section class='vh-100 gradient-custom'>"+
+                "<form action=\"EmployeeSubmitReimbursementServlet\" method=\"post\" > " +
+                "<div class='container py-5 h-100'>"+
+                "<div class='row d-flex justify-content-center align-items-center h-100'>"+
                 "<div class='col-12 col-md-9 col-lg-5 col-xl-5'>"+
-                    "<div class='card bg-dark text-white style ='border-radius: 8rem;'>"+
-                        "<div class='card-body p-5 text-center'>"+
+                "<div class='card bg-dark text-white style ='border-radius: 8rem;'>"+
+                "<div class='card-body p-5 text-center'>"+
 
-                            "<div class='mb-md-5 mt-md-4 pb-5'>"+
+                "<div class='mb-md-5 mt-md-4 pb-5'>"+
 
-                                "<h2 class='fw-bold mb-2 text'>Employee Reimbursement</h2>"+
-                                "<p class='text-white-50 mb-5'></p>"+
+                "<h2 class='fw-bold mb-2 text'>Employee Reimbursement</h2>"+
+                "<p class='text-white-50 mb-5'></p>"+
 
-                                "<div class='form-outline form-white mb-4'>"+
-                                    "<label class='form-label' for='detail' >Details</label>"+
-                                    "<input type='text' id='detail' class='form-control form-control-lg' name='detail' />"+
-                                "</div>"+
+                "<div class='form-outline form-white mb-4'>"+
+                "<label class='form-label' for='detail' >Details</label>"+
+                "<input type='text' id='detail' class='form-control form-control-lg' name='detail' />"+
+                "</div>"+
 
-                                "<div class='form-outline form-white mb-4'>"+
-                                    "<label class='form-label' for='amount'>Amount</label>"+
-                                    "<input type='number' id='amount' class='form-control form-control-lg' name='amount' />"+
-                                "</div>"+
+                "<div class='form-outline form-white mb-4'>"+
+                "<label class='form-label' for='amount'>Amount</label>"+
+                "<input type='number' id='amount' class='form-control form-control-lg' name='amount' />"+
+                "</div>"+
 
-                                "<div class='form-outline form-white mb-4'>"+
-                                "<label class='form-label' for='start' >Start Date:</label>"+
-                                "<input type='date' id='start' class='form-control form-control-lg' name='start' />"+
-                                "</div>"+
+                "<div class='form-outline form-white mb-4'>"+
+                "<label class='form-label' for='start' >Start Date:</label>"+
+                "<input type='date' id='start' class='form-control form-control-lg' name='start' />"+
+                "</div>"+
 
-                                 "<div class='form-outline form-white mb-4'>"+
-                                 "<label class='form-label' for='end' >End Date:</label>"+
-                                 "<input type='date' id='end' class='form-control form-control-lg' name='end' />"+
-                                 "</div>"+
+                "<div class='form-outline form-white mb-4'>"+
+                "<label class='form-label' for='end' >End Date:</label>"+
+                "<input type='date' id='end' class='form-control form-control-lg' name='end' />"+
+                "</div>"+
 
-                               "<button class='btn btn-outline-primary btn-lg px-10' type='submit'>Submit</button>"+
+                "<button class='btn btn-outline-primary btn-lg px-10' type='submit'>Submit</button>"+
 
-                            "</div>"+
+                "</div>"+
 
-                           " <div> <p class='mb-0'>Financial Managers</p> </div>"+
-
-                         "</div> </div> </div> </div> </div> </form> </section> ");
+                "</div> </div> </div> </div> </div> </form> </section> ");
 
     }
 
@@ -93,7 +90,7 @@ public class EmployeeSubmitReimbursementServlet extends HttpServlet {
         HttpSession httpSession = request.getSession();
         Employee employee = (Employee)httpSession.getAttribute("employee");
 
-        Integer ticketStatus = Reimbursement.hmap.get(("PENDING"));
+        Reimbursement.TicketStatus ticketStatus = Reimbursement.TicketStatus.PENDING;
         Reimbursement reimbursement = new Reimbursement();
         reimbursement.setExpenseDetail(expenseDetail);
         reimbursement.setAmount(amount);
@@ -102,14 +99,13 @@ public class EmployeeSubmitReimbursementServlet extends HttpServlet {
         reimbursement.setTicketStatus(ticketStatus);
         reimbursement.setEmployeeId(employee.getId());
 
-         reimbursementDao.insert(reimbursement);
+        reimbursementDao.insert(reimbursement);
 
-         out.println("<h1>You have submitted a new reimbursement request!</h1>");
+        out.println("<h1>You have submitted a new reimbursement request!</h1>");
 
-         // add html/css/bootstrap layout
+        // add html/css/bootstrap layout
 
 
 
     }
 
-}

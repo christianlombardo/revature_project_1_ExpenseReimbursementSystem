b5710cc6d2e1da3687d4bc881719cde99c872adb
joseph-malak <92759345+joseph-malak@users.noreply.github.com>
2021-11-21 15:40:15 -0500

    Update EmployeeViewPastRequestServlet.java

--- main/java/EmployeeViewPastRequestServlet.java
+++ main/java/EmployeeViewPastRequestServlet.java
@@ -23,20 +23,7 @@ public class EmployeeViewPastRequestServlet extends HttpServlet {
 
      out.println(
          
-                  "<ul class=\"nav nav-tabs\"navbar-fixed-top sticky>\n" +
-                        "  <li class=\"nav-item\">\n" +
-                        "    <a class=\"nav-link active\" aria-current=\"page\" href=\"#\">Home</a>\n" +
-                        "  </li>\n" +
-                        "  <li class=\"nav-item\">\n" +
-                        "    <a class=\"nav-link\" href=\"#\">Submit</a>\n" +
-                        "  </li>\n" +
-                        "  <li class=\"nav-item\">\n" +
-                        "    <a class=\"nav-link\" href=\"#\">View all current reimbursements</a>\n" +
-                        "  </li>\n" +
-                        "  <li class=\"nav-item\">\n" +
-                        "    <a class=\"nav-link\" href=\"#\">View all past reimbursements</a>\n" +
-                        "  </li>\n" +
-                        "</ul>" +
+              
                 "<style>body{background-color:grey}</style>" +
                 "<header>head</head>" +
                 "<div class=\"container\"" +

5d09b5543e0efa4959987458c06b2a841369dc00
joseph-malak <92759345+joseph-malak@users.noreply.github.com>
2021-11-23 19:41:35 -0500

    Update EmployeeViewPendingRequestsServlet.java

--- main/java/EmployeeViewPendingRequestsServlet.java
+++ main/java/EmployeeViewPendingRequestsServlet.java
@@ -20,15 +20,14 @@ public class EmployeeViewPendingRequestsServlet extends HttpServlet {
         request.getRequestDispatcher("test.html").include(request, response);
 
         out.println(
-
-                        "<style>body{background-color:grey}</style>" +
+                        "<style>body{background-color:#666699}</style>" +
                         "<div class=\"container\"" +
                         "<div class=\"row g-3\">" +
                         "<div class=\"col-md-12\">" +
                         "	<h1>Pending Reimbursements</h1>" +
                         "</div>" +
                         "<div class=\"col-md-12\">" +
-                        "<table class=\"table table-bordered table-dark table-responsive\" style=\"font-family:Arial;\">" +
+                        "<table class=\"table table-bordered table-dark table-responsive\" style=\">" +
                         "<tr>\n" +
                         "    <th>Ticket Number</th>\n" +
                         "    <th>Expense Detail</th>\n" +
